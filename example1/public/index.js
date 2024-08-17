44dcad37a4f090681e3d8c8da845977c1e28c85e
unknown <vasko.mike.dev@gmail.com>
2024-08-17 20:08:59 +0200

    index getLocalStream

--- example1/public/index.js
+++ example1/public/index.js
@@ -7,4 +7,31 @@ socket.on("connection-success", ({ socketId }) => {
   console.log(socketId);
 });
 
+const streamSuccess = async (stream) => {
+    localVideo.srcObject = stream
+    const track = stream.getVideoTracks()[0]
+    params = {
+      track,
+      ...params
+    }
+  }
+  
+const getLocalStream = () => {
+    navigator.getUserMedia({
+      audio: false,
+      video: {
+        width: {
+          min: 640,
+          max: 1920,
+        },
+        height: {
+          min: 400,
+          max: 1080,
+        }
+      }
+    }, streamSuccess, error => {
+      console.log(error.message)
+    })
+  }
+  
 btnLocalVideo.addEventListener("click", getLocalStream());
