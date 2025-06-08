e9daaa745b68e8a763219a7947aceffcf98b5cb1
Mike Vasko <vasko.mike.dev@gmail.com>
2025-06-08 21:42:20 +0200

    Remove server-app/dist from repository and respect .gitignore

--- server-app/dist/config/env.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-exports.env = {
-    // The port the server will listen on 
-    PORT: 3000,
-    //ListenIp address:
-    // LISTEN_IP: process.env.LISTEN_IP || '192.168.1.199',
-};
